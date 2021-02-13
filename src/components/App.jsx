8138696335181f519f316155393c229ff6d5c35a
rusiya <41211499+rusiya@users.noreply.github.com>
2021-02-12 21:41:11 -0800

    useState Hook Count++/--

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+//import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  // const state = useState(1);
+  // console.log(state);
+
+  // const RGB = [11, 2, 3];
+  // const [red, green, blue] = [11, 2, 3]; //destructure example
+  // console.log(red);
+
+  const [countFromHook, setCount] = useState(0); //setCount is the function.
+
+  function increase() {
+    //count++;
+    setCount(countFromHook + 1);
+  }
+
+  function decrease() {
+    //count++;
+    setCount(countFromHook - 1);
+  }
+
+  return (
+    <div className="container">
+      {/* <h1>{count}</h1> */}
+      {/* <h1>{state[0]}</h1> */}
+      <h1>{countFromHook}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
 }
 
 export default App;
