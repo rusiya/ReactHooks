6bee22b2fe9f12d95ecb064a915561bf2139ddfc
rusiya <41211499+rusiya@users.noreply.github.com>
2021-02-12 22:12:23 -0800

    useState Hook getTime and Second clock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,25 +9,43 @@ function App() {
   // const [red, green, blue] = [11, 2, 3]; //destructure example
   // console.log(red);
 
-  const [countFromHook, setCount] = useState(0); //setCount is the function.
+  // const [countFromHook, setCount] = useState(0); //setCount is the function.
 
-  function increase() {
-    //count++;
-    setCount(countFromHook + 1);
+  // function increase() {
+  //   //count++;
+  //   setCount(countFromHook + 1);
+  // }
+
+  // function decrease() {
+  //   //count++;
+  //   setCount(countFromHook - 1);
+  // }
+
+  // return (
+  //   <div className="container">
+  //     {/* <h1>{count}</h1> */}
+  //     {/* <h1>{state[0]}</h1> */}
+  //     <h1>{countFromHook}</h1>
+  //     <button onClick={increase}>+</button>
+  //     <button onClick={decrease}>-</button>
+  //   </div>
+  // );
+
+  const time = new Date().toLocaleTimeString();
+
+  const [timeHook, setTime] = useState(time);
+
+  function getTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
   }
 
-  function decrease() {
-    //count++;
-    setCount(countFromHook - 1);
-  }
+  setInterval(getTime, 1000); //Increase time every second without the button click.
 
   return (
     <div className="container">
-      {/* <h1>{count}</h1> */}
-      {/* <h1>{state[0]}</h1> */}
-      <h1>{countFromHook}</h1>
-      <button onClick={increase}>+</button>
-      <button onClick={decrease}>-</button>
+      <h1>{timeHook}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
